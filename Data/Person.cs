218a54af4b83f0e4fc997458f7beadda9617f850
Michael Conners <michael_conners@hotmail.com>
2021-06-23 17:30:14 -0400

    Added films collection to Person and display

--- Data/Person.cs
+++ Data/Person.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+
 
 namespace blazor.Data
 {
@@ -6,11 +8,9 @@ namespace blazor.Data
     {
         
         public string Name { get; set; }
-
         public string Birth_Year { get; set; }
-
         public string Gender {get; set;}
-
         public string Hair_Color { get; set; }
+        public List<string> Films {get; set;}
     }
 }
